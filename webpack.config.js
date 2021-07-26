2cc512d8ad5f1d8c453c06326503105d9e867fa3
AnSi <mailhouse@list.ru>
2021-07-27 00:13:03 +0400

    remove css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,6 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
     ],
   },
   devServer: {
