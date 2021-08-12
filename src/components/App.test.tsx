539c44ef1ee4f8315d62d8667da2153d786fb7a7
Andrey Sidorov <sidorov@uchi.ru>
2021-08-12 17:36:38 +0400

    Add click cell test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -42,4 +42,36 @@ describe("App", () => {
       screen.getByText(`Current cell: [${cols - 1},${rows - 1}]`)
     ).toBeInTheDocument();
   });
+  test("Click to the cell =>  auto-filling cells around the clicked", () => {
+    const [cols, rows] = [10, 10];
+    const cellProps = { size: 20, color: "#fff" };
+
+    render(<App size={{ cols, rows }} cell={cellProps} />);
+
+    const cells = screen.getAllByRole("cell");
+
+    // Center cell
+    const cell = cells[45];
+    const [x, y] = [
+      Number(cell.getAttribute("x")),
+      Number(cell.getAttribute("y")),
+    ];
+
+    fireEvent.click(cell, {});
+
+    const filledCells = cells.filter((c) => {
+      const [cX, cY] = [
+        Number(c.getAttribute("x")),
+        Number(c.getAttribute("y")),
+      ];
+
+      return (
+        (Math.abs(cX - x) == cellProps.size ||
+          Math.abs(cY - y) == cellProps.size) &&
+        c.getAttribute("fill") == "#000"
+      );
+    });
+
+    expect(filledCells.length).toBe(8);
+  });
 });
